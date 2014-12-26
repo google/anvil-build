5434624dc9801b48d4388113b7fe1beb851600ef
Joshua Harrison <joshharrison@gmail.com>
2014-12-26 08:15:18 -0800

    Complete the WorkUnit class and add comments.
    Added listener functionality to the WorkUnit class, allowing for an object to
    receive notifications when the WorkUnit's values are updated.
    
    Also finished commenting the WorkUnit class and adding tests for the listener
    functionality.

--- anvil/build_logging.py
+++ anvil/build_logging.py
@@ -7,37 +7,59 @@ from anvil import util
 
 class WorkUnit(object):
   """A class used to keep track of work progress.
+
+  The WorkUnit class uses total and complete metrics to keep track of work
+  completed. WorkUnits can be chained in parent-child relationships with any
+  updates to child WorkUnits being aggregated in the parent WorkUnit. The
+  total/complete numbers are used to determine the current status of a given
+  WorkUnit. Once an exception is set on a WorkUnit, its status automatically
+  becomes FAILED along with all units up the parent chain.
+
+  WorkUnits can also have change listeners attached. When the total, complete
+  or exception attributes are updated on a WorkUnit, all listeners on that
+  unit and all units up the parent chain are notified via a call to #update
+  on the listener.
   """
 
   def __init__(self, name):
-    self._completed = None
+    self._complete = None
     self._total = None
     self._waiting = True
     self._exception = None
 
     self.name = name
-    self.children = []
     self.parent = None
+    self.children = []
+    self.listeners = []
 
     self.start_time = None
     self.end_time = None
     
   @property
-  def completed(self):
-    completed = 0
+  def complete(self):
+    """Returns a complete count including all child WorkUnits.
+    """
+    complete = 0
     for child in self.children:
-      completed += child.completed
-    if self._completed:
-      completed += self._completed
-    return completed
+      complete += child.complete
+    if self._complete:
+      complete += self._complete
+    return complete
 
-  @completed.setter
-  def completed(self, completed):
-    self._completed = completed
-    self._validate_and_update_times()
+  @complete.setter
+  def complete(self, complete):
+    """Sets the complete count on this WorkUnit and validates all values.
+
+    Args:
+      complete: A number value corresponding to complete units of work.
+    """
+    self._complete = complete
+    self._validate_and_update('complete')
 
   @property
   def total(self):
+    """Returns a complete count including all parent WorkUnits.
+    """
     total = 0
     for child in self.children:
       total += child.total
@@ -47,11 +69,18 @@ class WorkUnit(object):
 
   @total.setter
   def total(self, total):
+    """Sets the total count on this WorkUnit and validates all values.
+
+    Args:
+      complete: A number value corresponding to total units of work.
+    """
     self._total = total
-    self._validate_and_update_times()
+    self._validate_and_update('total')
 
   @property
-  def exception(self):
+  def exception(self)
+    """Gets the exception value on this WorkUnit or any children.
+    """
     for child in self.children:
       if child.exception:
         return child.exception
@@ -59,42 +88,102 @@ class WorkUnit(object):
 
   @exception.setter
   def exception(self, exception):
+    """Sets an exception on this work unit and validates all values.
+
+    Args:
+      exception: A Python exception object.
+    """
     self._exception = exception
-    self._validate_and_update_times()
-
-  def _validate_and_update_times(self):
-    if (not self._total == None and
-        not self._completed == None and
-        self._completed > self._total):
-      raise ValueError('Completed tasks cannot be more than the total tasks.')
-    if not self.start_time:
-      self.start_time = util.timer()
-    if self.total == self.completed and not self.total == None:
-      self.end_time = util.timer()
-    self._waiting = False
-
-  def _is_waiting(self):
-    if self._waiting:
-      for child in self.children:
-        if not child._waiting:
-          self._waiting = False
-          break
-    return self._waiting
+    self._validate_and_update('exception')
 
   def get_status(self):
+    """Returns the status of this WorkUnit.
+
+    The WorkUnit is in a WAITING state until either its exception, total or
+    complete values are updated or any of its children change to a RUNNING
+    state. The WorkUnit immediately goes to a FAILED state if an exception is
+    set. If the total and completed units are both set to 0, the WorkUnit goes
+    to a SKIPPED state. If total units are set and are greater than the number
+    of completed units, it is set to a RUNNING state. Note that the complete
+    and total units used for this determination include those set on any child
+    WorkUnits. If total and complete are equal and greater than 0, then the
+    WorkUnit is set to SUCCEEDED.
+    """
     if self._is_waiting():
       return enums.Status.WAITING
     elif self.exception is not None:
       return enums.Status.FAILED
-    elif self.total == 0 and self.completed == 0:
+    elif self.total == 0 and self.complete == 0:
       return enums.Status.SKIPPED
 
-    diff = (self.total or 0) - (self.completed or 0)
+    diff = (self.total or 0) - (self.complete or 0)
     if diff > 0:
       return enums.Status.RUNNING
     elif diff == 0:
       return enums.Status.SUCCEEDED
 
   def add_child(self, child):
+    """Adds a child WorkUnit.
+
+    Args:
+      child: A child WorkUnit. The values of this child will be aggregated into
+          the values of this object.
+    """
     self.children.append(child)
     child.parent = self
+
+  def add_change_listener(self, listener):
+    """"Adds a change listener to this object.
+
+    Change listeners will receive notifications of updates to values on this
+    WorkUnit or any of its children. Listeners must implement the following
+    methods:
+      should_listen(WorkUnit):Boolean - Should return true if this listener
+          should listen to this WorkUnit. Gives a listener a chance to keep
+          from receiving updates for a given WorkUnit.
+      update(WorkUnit, String, *) - A method called when updates occur on a
+          WorkUnit. The method is called with the WorkUnit that changed, the
+          name of the attribute that was updated and the updated value.
+    """
+    if not listener.is_duplicate(self):
+      self.listeners.append(listener)
+
+  def _validate_and_update(self, attribute):
+    """Validates the current values on this WorkUnit and propegates updates.
+
+    This method validates the current WorkUnit and performs any updates that
+    occur as side-effects, such as updating the start and end time. This
+    method also calls change listeners and propagates change calls to parent
+    WorkUnits.
+
+    Args:
+      attribute: A string value corresponding to the name of the attribute
+          that was updated.
+    Raises:
+      ValueError: A ValueError is raised if any of the current values are
+          in an invalid state. For instance, if the completed count is greater
+          that the total count.
+    """
+    if (not self._total == None and
+        not self._complete == None and
+        self._complete > self._total):
+      raise ValueError('Complete tasks cannot be more than the total tasks.')
+    if not self.start_time:
+      self.start_time = util.timer()
+    if self.total == self.complete and not self.total == None:
+      self.end_time = util.timer()
+    self._waiting = False
+    for listener in self.listeners:
+      listener.update(self, attribute, getattr(self, attribute))
+    if self.parent:
+      self.parent._validate_and_update(attribute)
+
+  def _is_waiting(self):
+    """Returns true iff this WorkUnit and all its child units are WAITING.
+    """
+    if self._waiting:
+      for child in self.children:
+        if not child._waiting:
+          self._waiting = False
+          break
+    return self._waiting
--- anvil/build_logging_test.py
+++ anvil/build_logging_test.py
@@ -6,6 +6,9 @@ __author__ = 'joshharrison@google.com'
 
 import unittest2
 
+from mock import call
+from mock import MagicMock
+
 from anvil import build_logging
 from anvil import enums
 
@@ -17,8 +20,8 @@ class WorkUnitTest(unittest2.TestCase):
     self.assertEquals(enums.Status.WAITING, work_unit.get_status())
     work_unit.total = 10
     self.assertEquals(10, work_unit.total)
-    work_unit.completed = 10
-    self.assertEquals(10, work_unit.completed)
+    work_unit.complete = 10
+    self.assertEquals(10, work_unit.complete)
 
   def testAddChildUnit(self):
     child = build_logging.WorkUnit('child')
@@ -56,7 +59,7 @@ class WorkUnitTest(unittest2.TestCase):
 
   def testInvalidCountsThrowsError(self):
     work_unit = build_logging.WorkUnit('test')
-    work_unit.completed = 100
+    work_unit.complete = 100
     with self.assertRaises(ValueError):
       work_unit.total = 50
 
@@ -73,16 +76,53 @@ class WorkUnitTest(unittest2.TestCase):
     self.assertEquals(enums.Status.RUNNING, parent.get_status())
     parent.total = 30
     child.total = 0
-    child.completed = 0
+    child.complete = 0
     self.assertEquals(enums.Status.SKIPPED, child.get_status())
     self.assertEquals(enums.Status.RUNNING, parent.get_status())
-    parent.completed = 30
+    parent.complete = 30
     self.assertEquals(enums.Status.SUCCEEDED, parent.get_status())
 
     child.exception = ValueError()
     self.assertEquals(enums.Status.FAILED, child.get_status())
     self.assertEquals(enums.Status.FAILED, parent.get_status())
     self.assertEquals(child.exception, parent.exception)
+
+  def testChangeListenersCalled(self):
+    mock_listener = MagicMock(name='listener')
+    # If is_duplicate returns True, the listener will not be added to the
+    # WorkUnit, and will therefor not have its handler triggered on
+    # updates.
+    mock_listener.is_duplicate.return_value = True
+    test = build_logging.WorkUnit('test')
+    test.add_change_listener(mock_listener)
+
+    # Listeners should be triggered when updates occur anywhere along a WorkUnit
+    # tree.
+    child = build_logging.WorkUnit('child')
+    parent = build_logging.WorkUnit('parent')
+    parent.add_child(child)
+    mock_child_listener = MagicMock(name='child_listener')
+    mock_child_listener.is_duplicate.return_value = False
+    mock_parent_listener = MagicMock(name='parent_listener')
+    mock_parent_listener.is_duplicate.return_value = False
+    child.add_change_listener(mock_child_listener)
+    parent.add_change_listener(mock_parent_listener)
+
+    mock_child_listener.mock_calls = []
+    mock_parent_listener.mock_calls = []
+    child.total = 100
+    parent.total = 100
+    parent.complete = 100
+    expected_child_calls = [
+      call.update(child, 'total', 100)
+    ]
+    expected_parent_calls = [
+      call.update(parent, 'total', 100),
+      call.update(parent, 'total', 200),
+      call.update(parent, 'complete', 100)
+    ]
+    self.assertTrue(expected_child_calls == mock_child_listener.mock_calls)
+    self.assertTrue(expected_parent_calls == mock_parent_listener.mock_calls)
     
 
 if __name__ == '__main__':
